d2839079ddb760cea562ef1d2aef5ddf9a06c03c
SUMINMOON <sum95771@gmail.com>
2024-08-07 20:28:52 +0900

    style: 공통 스타일 설정(font, color)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,39 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        border: 'var(--border)',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        black100: '#17181A',
+        gray100: '#e5e7eb',
+        gray200: '#969697',
+        red100: '#EF4444',
+        red200: '#7f1d1d',
+      },
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: '300',
+        regular: '400',
+        semibold: '600',
+        bold: '700',
       },
     },
   },
