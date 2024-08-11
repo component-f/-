b02e22ee338a79c033b48e1e356be0eedd1b1053
SUMINMOON <sum95771@gmail.com>
2024-08-11 15:49:56 +0900

    feat: ModeToggle 모드 전환 기능, 폴더 위치 변경

---
+++ src/components/common/header/mode-toggle/index.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import { Sun, Moon } from 'lucide-react';
+
+/**
+ * 모드 전환 버튼 컴포넌트( light / dark )
+ */
+export default function ModeToggle() {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  const handleToggle = () => {
+    setTheme(currentTheme === 'dark' ? 'light' : 'dark');
+  };
+
+  return (
+    <>
+      <button onClick={handleToggle} className="w-8 h-8 p-2">
+        {theme === 'dark' ? <Sun size={20} /> : <Moon size={20} />}
+      </button>
+    </>
+  );
+}
