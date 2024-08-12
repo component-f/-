fb94b3d053c51d56209cf8f89ae375988d22162d
SUMINMOON <sum95771@gmail.com>
2024-08-12 21:11:56 +0900

    style: 공통 컬러 ring 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,7 @@ const config: Config = {
     extend: {
       colors: {
         border: 'hsl(var(--border))',
+        ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         primary: {
