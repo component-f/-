615fd67431944e6885c44b0d4281435844d25970
inji0212 <in17100170@kakao.com>
2024-08-14 14:35:18 +0900

    feat : button with icons

--- src/app/docs/components/button/page.tsx
+++ src/app/docs/components/button/page.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/ui/button'
+import { Delete, Send } from 'lucide-react'
 
 export default function Buttonpage() {
   return (
@@ -10,11 +11,13 @@ export default function Buttonpage() {
       <div className="flex flex-col gap-8 mt-12">
         <div className="flex flex-col gap-2">
           <label className="text-lg">Default</label>
-          <Button />
-          <Button variant="text"> Text</Button>
-          <Button variant="contained">Contained</Button>
-          <Button variant="outlined">Outlined</Button>
-          <Button disabled>Disabled</Button>
+          <div className="flex gap-2">
+            <Button />
+            <Button variant="text"> Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+            <Button disabled>Disabled</Button>
+          </div>
           <pre>
             <code>{`<Button />
 <Button variant="text"> Text</Button>
@@ -24,14 +27,22 @@ export default function Buttonpage() {
           </pre>
         </div>
         <div className="flex flex-col gap-2">
-          <label className="text-lg">Letter</label>
-          {/* <Avatar>A</Avatar> */}
-          {/* <Avatar className="bg-[#EF4444]">CF</Avatar> */}
-
+          <label className="text-lg">Button with Icons</label>
+          <div className="flex gap-2">
+            <Button variant="outlined" startIcon={<Delete />}>
+              Delete
+            </Button>
+            <Button variant="contained" endIcon={<Send size={16} />}>
+              Send
+            </Button>
+          </div>
           <pre>
-            <code>{`<Avatar>A</Avatar>
-<Avatar className="bg-[#EF4444]">CF</Avatar>
-`}</code>
+            <code>{`<Button variant="outlined" startIcon={<Delete />}>
+  Delete
+</Button>
+<Button variant="contained" endIcon={<Send size={16} />}>
+  Send
+</Button>`}</code>
           </pre>
         </div>
         <div className="flex flex-col gap-2">
--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -3,21 +3,26 @@ import { twMerge } from 'tailwind-merge'
 
 type TButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: 'text' | 'contained' | 'outlined'
+  startIcon?: React.ReactNode
+  endIcon?: React.ReactNode
 }
 
 const Button = React.forwardRef<HTMLButtonElement, TButtonProps>(
-  ({ className, variant = 'text', children, ...props }, ref) => {
-    const baseClasseName = 'w-[88px] h-[40px] rounded-[8px]  font-medium	duration-100 disabled:opacity-50'
+  ({ className, variant = 'text', startIcon, endIcon, children, ...props }, ref) => {
+    const baseClassName =
+      'py-2 px-4 rounded-[8px] font-medium flex justify-center items-center gap-2 duration-100 disabled:opacity-50'
 
     const variantClassNames = {
       text: 'border-none hover:opacity-50',
       contained: 'bg-primary text-background  hover:opacity-50',
-      outlined: 'bg-transparent border border-border hover:opacity-50',
+      outlined: ' border border-border hover:opacity-50',
     }
 
     return (
-      <button ref={ref} className={twMerge(baseClasseName, variantClassNames[variant], className)} {...props}>
+      <button ref={ref} className={twMerge(baseClassName, variantClassNames[variant], className)} {...props}>
+        {startIcon && <span className="start-icon mr-2">{startIcon}</span>}
         {children || 'BUTTON'}
+        {endIcon && <span className="end-icon ml-2">{endIcon}</span>}
       </button>
     )
   },
