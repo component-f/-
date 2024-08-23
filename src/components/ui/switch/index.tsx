64a66addcce16ed19903760ca285ee347677a48d
inji0212 <in17100170@kakao.com>
2024-08-23 12:45:34 +0900

    style: switch 스타일 수정

--- src/components/ui/switch/index.tsx
+++ src/components/ui/switch/index.tsx
@@ -4,9 +4,10 @@ import { twMerge } from 'tailwind-merge'
 
 type SwitchProps = {
   className?: string
+  id?: string
 }
 
-const Switch: React.FC<SwitchProps> = ({ className }) => {
+const Switch: React.FC<SwitchProps> = ({ className, id }) => {
   const [checked, setChecked] = useState(false)
 
   const handleChange = () => {
@@ -14,29 +15,31 @@ const Switch: React.FC<SwitchProps> = ({ className }) => {
   }
 
   return (
-    <label className="relative inline-flex items-center cursor-pointer">
-      <input type="checkbox" checked={checked} onChange={handleChange} className="sr-only" />
+    <div className="inline-flex items-center">
+      {/* 스위치 body */}
       <div
+        onClick={handleChange}
         className={twMerge(
-          'w-11 h-6 bg-gray-300 rounded-full transition-colors duration-200  ease-in-out',
-          checked ? 'bg-black' : 'bg-gray-300',
-          'w-[44px] h-[24px] rounded-full',
+          'relative flex items-center justify-start w-11 h-6 bg-[#E5E7EB] rounded-full transition-colors duration-200 ease-in-out',
+          checked ? 'bg-[hsl(var(--blacktogray200))]' : 'bg-[#E5E7EB]',
+          'w-[44px] h-[24px]',
           className,
         )}
       >
+        {/* 스위치 toggle */}
         <span
+          onClick={handleChange}
           className={twMerge(
-            'absolute bg-white rounded-full shadow-lg transition-transform duration-200 ease-in-out',
-            checked ? 'transform translate-x-[20px]' : 'transform translate-x-[-1px]',
+            'absolute bg-white rounded-full transition-transform duration-200 ease-in-out cursor-pointer shadow',
+            checked ? 'transform translate-x-[20px]' : 'transform translate-x-[0px]',
             'w-[26px] h-[26px]',
           )}
-          style={{
-            boxShadow: '0 1px 4px 0 rgba(12, 12, 13, 0.1)',
-            top: '-1px',
-          }}
         />
       </div>
-    </label>
+
+      {/* 체크박스 숨겨둠 */}
+      <input type="checkbox" id={id} checked={checked} onChange={handleChange} className="sr-only" />
+    </div>
   )
 }
 
