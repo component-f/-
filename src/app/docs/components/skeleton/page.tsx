ac49a5cca8df51b0d72dd6360cb9cdbb8adb7bdc
cyweyo <cyw7715@gmail.com>
2024-08-25 17:53:17 +0900

    feat: update skeleton page

--- src/app/docs/components/skeleton/page.tsx
+++ src/app/docs/components/skeleton/page.tsx
@@ -1,32 +1,107 @@
-import Skeleton from '@/components/ui/skeleton'
+'use client'
 
+import React, { useState, useEffect } from 'react'
+import Alert from '@/components/ui/alert'
+import * as Babel from '@babel/standalone'
+import { Ban } from 'lucide-react'
+import Skeleton from '@/components/ui/skeleton'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbText,
+} from '@/components/ui/breadcrumb'
+import {
+  Component,
+  ComponentContainer,
+  ComponentExample,
+  ComponentExampleCode,
+  ComponentExplain,
+  ComponentPropsTable,
+} from '@/components/common/component'
 export default function SkeletonPage() {
-  return (
+  const [code1, setCode1] = useState(`
     <>
-      <div className="mt-8 text-3xl font-bold">Skeleton</div>
-      <p className="mt-2 text-lg text-gray200">콘텐츠가 로딩되는 동안 플레이스홀더를 표시하는 데 사용됩니다.</p>
-      <div className="flex flex-col gap-8 mt-12">
-        <div className="flex flex-col gap-2">
-          <label className="text-lg">Default</label>
-          <div className="flex space-x-2">
-            <Skeleton className="rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-10 rounded-sm" />
-              <Skeleton className="h-4 w-40 rounded-sm" />
-            </div>
-          </div>
-          <pre>
-            <code>{`
-<div className="flex space-x-2">
-  <Skeleton className="rounded-full" />
-  <div className="space-y-2">
-    <Skeleton className="h-4 w-10 rounded-sm" />
-    <Skeleton className="h-4 w-40 rounded-sm" />
-  </div>
-</div>`}</code>
-          </pre>
+      <div className="flex space-x-2">
+        <Skeleton className="rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-10 rounded-sm" />
+          <Skeleton className="h-4 w-40 rounded-sm" />
         </div>
       </div>
+    </>
+    `)
+  const [RenderedComponent1, setRenderedComponent1] = useState<JSX.Element | null>(null)
+
+  useEffect(() => {
+    transformAndSetComponent(code1, setRenderedComponent1)
+  }, [code1])
+
+  const transformAndSetComponent = (
+    code: string,
+    setComponent: React.Dispatch<React.SetStateAction<JSX.Element | null>>,
+  ) => {
+    try {
+      const transformedCode = Babel.transform(code, {
+        presets: ['react'],
+      }).code
+
+      const Component = new Function('React', 'Skeleton', `return ${transformedCode};`)
+
+      const element = Component(React, Skeleton, Ban)
+
+      setComponent(element)
+    } catch (error) {
+      console.error('Error rendering component:', error)
+      setComponent(
+        <Alert className="w-1/3 bg-red-500 text-white" title="오류" icon={<Ban size={35} />}>
+          컴포넌트를 렌더링 하는 데 실패했습니다.
+        </Alert>,
+      )
+    }
+  }
+  return (
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/docs">Docs</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbText>Skeleton</BreadcrumbText>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <Component>
+        <ComponentExplain
+          title="Skeleton"
+          description="콘텐츠가 로딩되는 동안 플레이스홀더를 표시하는 데 사용됩니다."
+        />
+        <ComponentContainer>
+          <ComponentExample>{RenderedComponent1}</ComponentExample>
+          <ComponentExampleCode code={code1} setCode={setCode1} />
+        </ComponentContainer>
+      </Component>
+
+      <ComponentPropsTable
+        description="Child로 전달되는 내용은 <Alert> 컴포넌트가 사용자에게 표시할 주된 메시지입니다."
+        props={[
+          {
+            prop: 'className',
+            type: 'string',
+            default: '',
+            description: '컴포넌트에 CSS 클래스를 추가하여 스타일을 지정하는 데 사용됩니다.',
+          },
+        ]}
+      />
     </>
   )
 }
