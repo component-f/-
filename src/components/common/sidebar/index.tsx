f9b343d3673ba8c7bc9f889a3bfaad5f708d6fb0
inji0212 <in17100170@kakao.com>
2024-08-26 13:20:40 +0900

    style : 보더라인 삭제

--- src/components/common/sidebar/index.tsx
+++ src/components/common/sidebar/index.tsx
@@ -10,7 +10,7 @@ export default function Sidebar() {
   const componentPaths = Object.entries(PATH).filter(([, path]) => path.startsWith('/docs/components'))
 
   return (
-    <div className="flex flex-col space-y-1 w-[200px] p-4 border-r h-full min-h-screen">
+    <div className="flex flex-col space-y-1 w-[200px] p-4 h-full min-h-screen">
       <h1 className="font-bold">Components</h1>
       {componentPaths.map(([key, path]) => {
         const isActive = pathname === path
