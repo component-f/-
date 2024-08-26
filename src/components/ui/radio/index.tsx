95de80cd6b50a5e5ea623edd97247c0f57f1efbf
lsm0323 <gwanggong217@gmail.com>
2024-08-26 15:25:31 +0900

    feat:radio 라디오 기능 추가 및 수정

--- src/components/ui/radio/index.tsx
+++ src/components/ui/radio/index.tsx
@@ -4,14 +4,24 @@ interface RadioProps {
   label: string
   name: string
   value: string
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  defaultChecked?: boolean
   disabled?: boolean
 }
 
-const Radio: React.FC<RadioProps> = ({ label, name, value, disabled = false }) => {
+const Radio: React.FC<RadioProps> = ({ label, name, value, onChange, defaultChecked = false, disabled = false }) => {
   return (
-    <label className={`inline-flex items-center mt-3 mb-2 mr-4 ${disabled ? 'cursor-not-allowed opacity-20' : ''}`}>
-      <input type="radio" name={name} value={value} disabled={disabled} className="h-5 w-5" />
-      <span className="ml-2 peer">{label}</span>
+    <label className={`block peer items-center mt-3 mb-2 mr-4 ${disabled ? `text-gray-200 cursor-not-allowed` : ''}`}>
+      <input
+        type="radio"
+        name={name}
+        value={value}
+        onChange={onChange}
+        defaultChecked={defaultChecked}
+        disabled={disabled}
+        className="peer form-radio h-5 w-5"
+      />
+      <span className="ml-2">{label}</span>
     </label>
   )
 }
