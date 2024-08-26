368ca1fa5d80edd75d690cf137081d55f299005c
inji0212 <in17100170@kakao.com>
2024-08-26 13:17:05 +0900

    style : 현재 링크 강조 추가

--- src/components/common/sidebar/index.tsx
+++ src/components/common/sidebar/index.tsx
@@ -2,19 +2,29 @@
 
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { PATH } from '@/constants/path'
 
 export default function Sidebar() {
+  const pathname = usePathname()
   const componentPaths = Object.entries(PATH).filter(([, path]) => path.startsWith('/docs/components'))
 
   return (
     <div className="flex flex-col space-y-1 w-[200px] p-4 border-r h-full min-h-screen">
       <h1 className="font-bold">Components</h1>
-      {componentPaths.map(([key, path]) => (
-        <Link key={key} href={path} replace className="hover:text-border capitalize block">
-          {key}
-        </Link>
-      ))}
+      {componentPaths.map(([key, path]) => {
+        const isActive = pathname === path
+        return (
+          <Link
+            key={key}
+            href={path}
+            replace
+            className={`capitalize block hover:text-foreground ${isActive ? 'font-bold text-foreground' : 'text-gray-400'}`}
+          >
+            {key}
+          </Link>
+        )
+      })}
     </div>
   )
 }
