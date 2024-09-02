9d6bedccb6d2d7b24c6846613cd485f2cf26ad43
SUMINMOON <sum95771@gmail.com>
2024-09-02 15:41:56 +0900

    feat: docs page layout에 breadcrumb 추가

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Sidebar from '@/components/common/sidebar'
+import BreadCrumb from '@/components/common/bread-crumb'
 import '@/styles/globals.css'
 
 export const metadata = {
@@ -9,11 +10,14 @@ export const metadata = {
 
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex container grow mt-4">
+    <div className="flex grow mt-4">
       <div className="ml-0 sm:ml-0 md:ml-4 lg:ml-12 xl:ml-16">
         <Sidebar />
       </div>
-      <main className="grow ml-4 mr-0 sm:mr-0 md:mr-4 lg:mr-12 xl:mr-16">{children}</main>
+      <main className="grow ml-4 mr-0 sm:mr-0 md:mr-4 lg:mr-12 xl:mr-16">
+        <BreadCrumb />
+        <div className="pb-14">{children}</div>
+      </main>
     </div>
   )
 }
