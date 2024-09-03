63fa7e00961f73691aa680b664c58d8c071f501c
lsm0323 <gwanggong217@gmail.com>
2024-09-03 21:01:59 +0900

    refactor:select css 변경 및 페이지 수정

--- src/components/ui/select/index.tsx
+++ src/components/ui/select/index.tsx
@@ -31,7 +31,7 @@ const Select: React.FC<SelectProps> = ({ options, onSelect, className, defaultSe
   return (
     <div className={`relative inline-block w-64 ${className}`}>
       <button
-        className="w-full px-4 py-2 text-left bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-200"
+        className="w-full px-4 py-2 text-left bg-white border border-border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-200 text-sm"
         onClick={() => setIsOpen(!isOpen)}
       >
         {options.find((option) => option.value === selectedValue)?.label}
@@ -43,13 +43,13 @@ const Select: React.FC<SelectProps> = ({ options, onSelect, className, defaultSe
       </button>
 
       {isOpen && (
-        <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg">
-          <ul className="py-1">
+        <div className="absolute z-10 w-full mt-1 bg-white border border-border rounded-md shadow-lg">
+          <ul className="py-1 text-sm">
             {options.map((option) => (
               <li
                 key={option.value}
                 className={`cursor-pointer px-4 py-2 ${
-                  option.value === selectedValue ? 'bg-gray-100' : ''
+                  option.value === selectedValue ? 'bg-gray-200' : ''
                 } hover:bg-gray-200`}
                 onClick={() => handleSelect(option.value)}
               >
