298802b9176dc3907740c2fae61e19463c9d213b
SUMINMOON <sum95771@gmail.com>
2024-09-06 19:23:54 +0900

    feat: header 상단 고정

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -12,7 +12,7 @@ import List from './list'
  */
 export default function Header() {
   return (
-    <header className="container h-14 flex items-center justify-between">
+    <header className="sticky top-0 z-40 h-14 flex items-center justify-between bg-background px-4 md:px-6">
       <nav>
         <ul className="text-sm text-gray200 font-medium flex items-center gap-4">
           <List path={PATH.root}>
