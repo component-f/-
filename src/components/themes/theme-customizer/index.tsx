1c53a8830d2d24c9aceb33dedf43f08082bd0412
SUMINMOON <sum95771@gmail.com>
2024-09-06 14:16:46 +0900

    style: 임시 변경

--- src/components/themes/theme-customizer/index.tsx
+++ src/components/themes/theme-customizer/index.tsx
@@ -10,7 +10,7 @@ import RecentActivity from '@/components/examples/recent-activity'
 import ReportForm from '@/components/examples/report-form'
 import SignUpForm from '@/components/examples/signup-form'
 import Todo from '@/components/examples/to-do'
-import Button from '@/components/ui/button'
+import { Button } from '@/components/ui/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown'
 import { ThemeWrapper } from '../theme-wrapper'
 import { useConfig } from '@/hooks/use-config'
@@ -34,46 +34,44 @@ export default function ThemeCustomizer() {
   return (
     <>
       <div className="flex justify-between w-full relative">
-        <div className="">
-          install
-          <div></div>
-        </div>
-        <div className="">
+        <div>install</div>
+        <div className="w-2/3">
+          <DropdownMenu>
+            <DropdownMenuTrigger toggleStatusBar={handleToggle} buttonRef={buttonRef}>
+              <Button variant="contained" className="mb-4">
+                Customize
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              showStatusBar={open}
+              toggleStatusBar={handleToggle}
+              menuRef={menuRef}
+              buttonRef={buttonRef}
+            >
+              {themes.map((theme, index) => {
+                const activeColor = theme.activeColor[currentTheme as 'light' | 'dark']
+
+                return (
+                  <DropdownMenuItem key={index}>
+                    <span
+                      className={cn('flex h-5 w-5 items-center justify-center rounded-full mr-2')}
+                      style={{ backgroundColor: `hsl(${activeColor})` }}
+                    ></span>
+                    <div onClick={() => setTheme(`${theme.name}`)}>{theme.name}</div>
+                  </DropdownMenuItem>
+                )
+              })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+
           <ThemeWrapper>
-            <DropdownMenu>
-              <DropdownMenuTrigger toggleStatusBar={handleToggle} buttonRef={buttonRef}>
-                <Button variant="contained">Customize</Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                showStatusBar={open}
-                toggleStatusBar={handleToggle}
-                menuRef={menuRef}
-                buttonRef={buttonRef}
-              >
-                {themes.map((theme, index) => {
-                  const activeColor = theme.activeColor[currentTheme as 'light' | 'dark']
-
-                  return (
-                    <DropdownMenuItem key={index}>
-                      <span
-                        className={cn('flex h-5 w-5 items-center justify-center rounded-full mr-2')}
-                        style={{ backgroundColor: `hsl(${activeColor})` }}
-                      ></span>
-                      <div onClick={() => setTheme(`${theme.name}`)}>{theme.name}</div>
-                    </DropdownMenuItem>
-                  )
-                })}
-              </DropdownMenuContent>
-            </DropdownMenu>
-
-            <Button onClick={() => setTheme('orange')}>orange</Button>
-            <div className="flex gap-4 justify-between absolute left-1/2">
-              <div className="flex flex-col gap-4 w-[480px]">
+            <div className="flex gap-4 justify-between">
+              <div className="flex flex-col gap-4">
                 <SignUpForm />
                 <Chat />
                 <Notifications />
               </div>
-              <div className="flex flex-col gap-4 w-[312px]">
+              <div className="flex flex-col gap-4">
                 <RecentActivity />
                 <Todo />
                 <ReportForm />
