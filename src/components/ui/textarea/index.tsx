5dd4d56d8e5a2b521ee06325c24a2ee472b078f5
cyweyo <cyw7715@gmail.com>
2024-09-07 20:41:24 +0900

    style: css 수정

--- src/components/ui/textarea/index.tsx
+++ src/components/ui/textarea/index.tsx
@@ -8,7 +8,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TTextareaProps>(({ classN
     <textarea
       ref={ref}
       className={twMerge(
-        `border text-sm placeholder:text-gray200 focus-visible:outline-none  focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 rounded-lg w-full h-[100px] pt-[8px] pl-[16px] bg-transparent`,
+        `border text-sm placeholder:text-gray200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 rounded-lg w-full h-[100px] pt-[8px] pl-[16px] bg-transparent`,
         className,
       )}
       {...props}
