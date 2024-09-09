762a477a1dc9d27a1bbc2b92579d7832b1cd189d
cyweyo <cyw7715@gmail.com>
2024-09-09 20:48:22 +0900

    feat: add copyToClipboar page.tsx에서 사용위함

---
+++ src/components/common/copyToClipboard/index.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useState } from 'react'
+import { Copy, CopyCheck } from 'lucide-react'
+
+export default function CopyToClipboard({ code }: { code: string }) {
+  const [copied, setCopied] = useState(false)
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(code)
+    setCopied(true)
+    setTimeout(() => setCopied(false), 1500)
+  }
+
+  return (
+    <div className="flex items-center justify-between bg-foreground text-background py-2 px-4 rounded-lg w-[500px] mt-2">
+      <code>{code}</code>
+
+      <div className="flex items-center justify-center w-[20px]">
+        {copied ? (
+          <CopyCheck size={20} className="text-[#0090FF]" />
+        ) : (
+          <Copy size={15} onClick={handleCopy} className="hover:text-[#0090FF] cursor-pointer" />
+        )}
+      </div>
+    </div>
+  )
+}
