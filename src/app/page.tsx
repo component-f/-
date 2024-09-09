bcd2287969277561fb3eb28f5aec6280a371cba1
cyweyo <cyw7715@gmail.com>
2024-09-09 23:39:21 +0900

    refactor: edit main page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ThemeCustomizer from '@/components/themes/theme-customizer'
 
 export default function HomePage() {
   return (
-    <div className="container relative">
+    <div className="relative">
       <ThemeCustomizer />
     </div>
   )
