ae2cd426936da7fa05296768317b0e897574ed08
inji0212 <in17100170@kakao.com>
2024-09-10 10:32:02 +0900

    feat : drawer메뉴 추가

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -7,11 +7,10 @@ export const metadata = {
   title: 'Docs - Component-Factory',
   description: '컴포넌트 UI 라이브러리 문서 페이지입니다.',
 }
-
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex grow mt-4">
-      <div className="ml-0 sm:ml-0 md:ml-4 lg:ml-12 xl:ml-16">
+      <div className="ml-0 sm:ml-0 md:ml-4 lg:ml-12 xl:ml-16 hidden md:block">
         <Sidebar />
       </div>
       <main className="grow ml-4 mr-0 sm:mr-0 md:mr-4 lg:mr-12 xl:mr-16">
