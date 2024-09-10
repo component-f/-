db7bb25e4d0baeffea05f948cc4338f7c7dce858
SUMINMOON <sum95771@gmail.com>
2024-09-10 16:30:40 +0900

    refactor: usage-page로 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Usage from '@/components/common/usage'
 import ThemeCustomizer from '@/components/themes/theme-customizer'
 
 export default function HomePage() {
   return (
     <div className="relative">
+      <Usage />
       <ThemeCustomizer />
     </div>
   )
--- src/components/themes/theme-customizer/index.tsx
+++ src/components/themes/theme-customizer/index.tsx
@@ -3,14 +3,13 @@ import * as React from 'react'
 import { useTheme } from 'next-themes'
 
 import '@/styles/themes.css'
+import { cn } from '@/utils/cn'
 
 import { Button } from '@/components/ui/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown'
 import { ThemeWrapper } from '../theme-wrapper'
 import { useConfig } from '@/hooks/use-config'
 import { themes } from '@/constants/theme'
-import { cn } from '@/utils/cn'
-import Usage from '@/components/common/usage'
 import SignUpForm from '@/components/examples/signup-form'
 import CookieSettings from '@/components/examples/cookie-settings'
 import ReportForm from '@/components/examples/report-form'
@@ -36,62 +35,56 @@ export default function ThemeCustomizer() {
 
   const { setTheme } = useConfig()
   return (
-    <>
-      <div className="w-[400px] mt-6 xl:[550px] xl:m-6 xl:fixed xl:h-1/3 xl:transform xl:translate-y-1/3 z-40">
-        <Usage />
-      </div>
+    <div className="w-full xl:w-2/3 mt-4 xl:mt-0 xl:pl-8 xl:absolute xl:right-0 xl:top-0">
+      <DropdownMenu>
+        <DropdownMenuTrigger toggleStatusBar={handleToggle} buttonRef={buttonRef}>
+          <Button variant="contained" className="mb-4">
+            Customize
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          showStatusBar={open}
+          toggleStatusBar={handleToggle}
+          menuRef={menuRef}
+          buttonRef={buttonRef}
+        >
+          {themes.map((theme, index) => {
+            const activeColor = theme.activeColor[currentTheme as 'light' | 'dark']
 
-      <div className="w-full xl:w-2/3 mt-4 xl:mt-0 xl:pl-8 xl:absolute xl:right-0 xl:top-0">
-        <DropdownMenu>
-          <DropdownMenuTrigger toggleStatusBar={handleToggle} buttonRef={buttonRef}>
-            <Button variant="contained" className="mb-4">
-              Customize
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            showStatusBar={open}
-            toggleStatusBar={handleToggle}
-            menuRef={menuRef}
-            buttonRef={buttonRef}
-          >
-            {themes.map((theme, index) => {
-              const activeColor = theme.activeColor[currentTheme as 'light' | 'dark']
+            return (
+              <DropdownMenuItem key={index}>
+                <div className="flex items-center justify-center" onClick={() => setTheme(`${theme.name}`)}>
+                  <span
+                    className={cn('h-5 w-5 rounded-full mr-2')}
+                    style={{ backgroundColor: `hsl(${activeColor})` }}
+                  ></span>
+                  <div>{theme.name}</div>
+                </div>
+              </DropdownMenuItem>
+            )
+          })}
+        </DropdownMenuContent>
+      </DropdownMenu>
 
-              return (
-                <DropdownMenuItem key={index}>
-                  <div className="flex items-center justify-center" onClick={() => setTheme(`${theme.name}`)}>
-                    <span
-                      className={cn('h-5 w-5 rounded-full mr-2')}
-                      style={{ backgroundColor: `hsl(${activeColor})` }}
-                    ></span>
-                    <div>{theme.name}</div>
-                  </div>
-                </DropdownMenuItem>
-              )
-            })}
-          </DropdownMenuContent>
-        </DropdownMenu>
-
-        <ThemeWrapper>
-          <div className="grid grid-cols-[repeat(auto-fill,_minmax(400px,_1fr))] gap-4">
-            <div className="space-y-4">
-              <SignUpForm />
-              <CookieSettings />
-              <FnQ />
-            </div>
-            <div className="space-y-4">
-              <ReportForm />
-              <RecentActivity />
-              <TeamMembers />
-              <Chat />
-            </div>
-            <div className="space-y-4">
-              <Todo />
-              <Notifications />
-            </div>
+      <ThemeWrapper>
+        <div className="grid grid-cols-[repeat(auto-fill,_minmax(400px,_1fr))] gap-4">
+          <div className="space-y-4">
+            <SignUpForm />
+            <CookieSettings />
+            <FnQ />
           </div>
-        </ThemeWrapper>
-      </div>
-    </>
+          <div className="space-y-4">
+            <ReportForm />
+            <RecentActivity />
+            <TeamMembers />
+            <Chat />
+          </div>
+          <div className="space-y-4">
+            <Todo />
+            <Notifications />
+          </div>
+        </div>
+      </ThemeWrapper>
+    </div>
   )
 }
