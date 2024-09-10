4e2c59b2d7feedbb07be33bf0352cd9cb12af009
SUMINMOON <sum95771@gmail.com>
2024-09-10 20:52:55 +0900

    feat: HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Usage from '@/components/common/usage'
+import ThemeExamples from '@/components/themes/theme-examples'
 import ThemeCustomizer from '@/components/themes/theme-customizer'
 
 export default function HomePage() {
   return (
-    <div className="relative">
-      <Usage />
-      <ThemeCustomizer />
+    <div className="w-full md:flex">
+      <div className="mt-32 mb-6 px-20">
+        <Usage />
+        <ThemeCustomizer />
+      </div>
+      <div className="px-20 md:px-0">
+        <ThemeExamples />
+      </div>
     </div>
   )
 }
